f8270914cc963339eae664752eb55d7026da1707
yanfer <yanfer.daniel@gmail.com>
2022-07-16 16:48:01 -0500

    logo update

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        logo
+        <Logo/>
       </div>
     );
   }
---
+++ src/components/logo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+  constructor(){ 
+    super();
+  }
+
+  render() {
+    return (
+      <div className="logo-main">
+        <img src='/assets/ds_circle_logo.png'/>
+      </div>
+    );
+  }
+}
