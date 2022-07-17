716bbfc9b96e466b2b441df53ac204d508732ee6
yanfer <yanfer.daniel@gmail.com>
2022-07-16 23:19:14 -0500

    created Results page

---
+++ src/components/results.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+
+export default class Results extends Component {
+  render() {
+    return (
+      <div> 
+        <h1>Results</h1>
+        <Logo/>
+      </div>
+    );
+  }
+}
