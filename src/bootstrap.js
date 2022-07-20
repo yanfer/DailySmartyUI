98f29d066d628565a5932fe161309b2bcd748a5b
yanfer <yanfer.daniel@gmail.com>
2022-07-19 23:57:18 -0500

    connect recent posts to redux

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 import "./style/main.scss";
 
--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class RecentPosts extends Component {
+import * as actions from '../actions';
+
+class RecentPosts extends Component {
+
+  componentDidMount(){
+    this.props.fetchRecentPosts();
+  }
   render() {
     return (
       <div className='recent-posts'>
@@ -18,3 +25,5 @@ export default class RecentPosts extends Component {
     );
   }
 }
+
+export default connect(null, actions)(RecentPosts)
