4a77eae2ef718c708d3adca8d0b08de99466d4be
yanfer <yanfer.daniel@gmail.com>
2022-07-22 19:57:52 -0500

    checking posts with redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
+
+import posts from './postsReducer';
 const rootReducer = combineReducers({
-  form
+  form,
+  posts
 });
 
 export default rootReducer;
