943236a9c8662224e44e5af65aeeff026a8af999
yanfer <yanfer.daniel@gmail.com>
2022-07-27 01:10:31 -0500

    styled results component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -17,7 +17,7 @@ class SearchBar extends Component {
       const { handleSubmit } = this.props;
 
       return (
-          <form className="search-bar" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <form className={`search-bar search-bar-${this.props.page}`} onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
             <div className="search-bar-wrapper">
               <Field name="query" component={this.renderInput}/>
               <p>Press return to search</p>
